46d3fce652f4169b3bcefa6fe5cb9e52a7d13750
reneafranco <reneadonayfranco@gmail.com>
2025-08-29 14:54:04 -0400

    cors

--- fronted/rental-equipment-app-fronted/src/app/services/api-configuration.ts
+++ fronted/rental-equipment-app-fronted/src/app/services/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8088/api/v1';
+  rootUrl: string = 'http://52.90.96.138:8088/api/v1';
 }
 
 /**
